6f336359e500fdecc98923fc1c7b049f2725f4ee
unknown <biradar.abhijit@gmail.com>
2022-09-02 13:49:43 +0530

    2-Sept-2022

--- com.dsa/src/main/java/com/dsa/sorting/BinaryInsertionSort.java
+++ com.dsa/src/main/java/com/dsa/sorting/BinaryInsertionSort.java
@@ -1,6 +1,37 @@
 package com.dsa.sorting;
 
-public class BinaryInsertionSort {
-	// Refer
-	// https://www.geeksforgeeks.org/binary-insertion-sort/
+// Refer
+// https://www.geeksforgeeks.org/binary-insertion-sort/
+
+import java.util.Arrays;
+
+class BinaryInsertionSort {
+
+	public static void main(String[] args) {
+		final int[] arr = { 37, 23, 0, 17, 12, 72, 31, 46, 100, 88, 54 };
+
+		new BinaryInsertionSort().sort(arr);
+
+		for (int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
+	}
+
+	// Driver Code
+	public void sort(int array[]) {
+		for (int i = 1; i < array.length; i++) {
+			int x = array[i];
+
+			// Find location to insert
+			// using binary search
+			int j = Math.abs(Arrays.binarySearch(array, 0, i, x) + 1);
+
+			// Shifting array to one
+			// location right
+			System.arraycopy(array, j, array, j + 1, i - j);
+
+			// Placing element at its
+			// correct location
+			array[j] = x;
+		}
+	}
 }
--- com.dsa/src/main/java/com/dsa/stack/StackUsingArray.java
+++ com.dsa/src/main/java/com/dsa/stack/StackUsingArray.java
@@ -8,62 +8,76 @@ class StackUsingArray {
 	private int top;
 	private int capacity;
 
-// Creating a stack
+	// Creating a stack
 	StackUsingArray(int size) {
 		arr = new int[size];
 		capacity = size;
 		top = -1;
 	}
 
-// Add elements into stack
+	// Check if the stack is full
+	public Boolean isFull() {
+		return top == capacity - 1;
+	}
+
+	// Add elements into stack
 	public void push(int x) {
 		if (isFull()) {
-			System.out.println("OverFlow\nProgram Terminated\n");
+			System.out.println("Stack is already full!");
 			System.exit(1);
 		}
 
-		System.out.println("Inserting " + x);
+		System.out.println("Inserting " + x + " into stack.");
 		arr[++top] = x;
 	}
 
-// Remove element from stack
+	// Check if the stack is empty
+	public Boolean isEmpty() {
+		return top == -1;
+	}
+
+	// Remove element from stack
 	public int pop() {
 		if (isEmpty()) {
-			System.out.println("STACK EMPTY");
+			System.out.println("Stack is empty!");
 			System.exit(1);
 		}
 		return arr[top--];
 	}
 
-// Utility function to return the size of the stack
+	// Utility function to return the size of the stack
 	public int size() {
 		return top + 1;
 	}
 
-// Check if the stack is empty
-	public Boolean isEmpty() {
-		return top == -1;
-	}
-
-// Check if the stack is full
-	public Boolean isFull() {
-		return top == capacity - 1;
-	}
-
 	public void printStack() {
+		System.out.println("Displaying Stack elements:");
+		
 		for (int i = 0; i <= top; i++) {
-			System.out.println(arr[i]);
+			System.out.print(arr[i] + " ");
 		}
+		System.out.println();		
 	}
 
 	public static void main(String[] args) {
 		StackUsingArray stack = new StackUsingArray(5);
 
+		System.out.println("Is Stack empty: " + stack.isEmpty());
+		
+		System.out.println("Is Stack full: " + stack.isFull());
+		
 		stack.push(1);
+		stack.printStack();
+		
 		stack.push(2);
+		stack.printStack();
+		
 		stack.push(3);
+		stack.printStack();
+		
 		stack.push(4);
-
+		stack.printStack();
+		
 		stack.pop();
 		System.out.println("\nAfter popping out");
 
