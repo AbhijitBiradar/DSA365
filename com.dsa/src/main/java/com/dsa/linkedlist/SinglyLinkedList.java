0dadf7dcde8e097c0e4c0107ac598dcc6e9df390
unknown <biradar.abhijit@gmail.com>
2022-09-09 11:33:00 +0530

    09-sept-2022 commit

--- com.dsa/src/main/java/com/dsa/linkedlist/SinglyLinkedList.java
+++ com.dsa/src/main/java/com/dsa/linkedlist/SinglyLinkedList.java
@@ -45,6 +45,16 @@ public class SinglyLinkedList {
 		System.out.println();
 	}
 
+	public void display(ListNode node) {
+		ListNode currentNode = node;
+		while (currentNode != null) {
+			System.out.print(currentNode.data + " --> ");
+			currentNode = currentNode.next;
+		}
+		System.out.print("null");
+		System.out.println();
+	}
+
 	public int length() {
 		if (head == null) {
 			return 0;
@@ -85,23 +95,23 @@ public class SinglyLinkedList {
 		// 1 -> 4 -> 5
 		// 1 -> 6 -> 4 -> 5
 		// position starts from 1
-		ListNode node = new ListNode(value);
+		ListNode newNode = new ListNode(value);
 
 		if (position == 1) {
-			node.next = head;
-			head = node;
+			newNode.next = head;
+			head = newNode;
 		} else {
-			ListNode previous = head;
-			int count = 1; // position - 1
+			ListNode previousNode = head;
+			int count = 1;
 
 			while (count < position - 1) {
-				previous = previous.next;
+				previousNode = previousNode.next;
 				count++;
 			}
 
-			ListNode current = previous.next;
-			previous.next = node;
-			node.next = current;
+			ListNode currentNode = previousNode.next;
+			previousNode.next = newNode;
+			newNode.next = currentNode;
 		}
 
 		System.out.println("Node inserted at " + position + " successfully!");
@@ -111,13 +121,14 @@ public class SinglyLinkedList {
 		if (head == null) {
 			return null;
 		}
-		ListNode temp = head;
+
+		ListNode tempNode = head;
 		head = head.next;
-		temp.next = null;
+		tempNode.next = null;
 
 		System.out.println("Node deleted from first successfully!");
 
-		return temp;
+		return tempNode;
 	}
 
 	public ListNode deleteLast() {
@@ -126,23 +137,23 @@ public class SinglyLinkedList {
 		}
 
 		if (head.next == null) {
-			ListNode temp = head;
+			ListNode tempNode = head;
 			head = head.next;
-			return temp;
+			return tempNode;
 		}
 
-		ListNode current = head;
-		ListNode previous = null;
+		ListNode currentNode = head;
+		ListNode previousNode = null;
 
-		while (current.next != null) {
-			previous = current;
-			current = current.next;
+		while (currentNode.next != null) {
+			previousNode = currentNode;
+			currentNode = currentNode.next;
 		}
-		previous.next = null; // break the chain
+		previousNode.next = null; // break the chain
 
 		System.out.println("Node deleted from last successfully!");
 
-		return current;
+		return currentNode;
 	}
 
 	public void delete(int position) {
@@ -172,45 +183,47 @@ public class SinglyLinkedList {
 			return false;
 		}
 
-		ListNode current = head;
-		while (current != null) {
-			if (current.data == searchKey) {
-
+		ListNode currentNode = head;
+		while (currentNode != null) {
+			if (currentNode.data == searchKey) {
 				System.out.println("Given node is present in Linked List!");
-
 				return true;
 			}
-			current = current.next;
+			currentNode = currentNode.next;
 		}
 
 		System.out.println("Given node is not present in Linked List!");
 		return false;
 	}
 
-	// code not working watch video
-	public ListNode reverse() {
+	// Refer
+	// https://www.youtube.com/watch?v=jY-EUKXYT20&list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&index=36
+	// Not understood
+
+	public ListNode reverse(ListNode head) {
 		if (head == null) {
 			System.out.println("Linked List is empty!");
 			return null;
 		}
 
-		ListNode current = head;
-		ListNode previous = null;
-		ListNode next = null;
+		ListNode currentNode = head;
+		ListNode previousNode = null;
+		ListNode nextNode = null;
 
-		while (current != null) {
-			next = current.next;
-			current.next = previous;
-			previous = current;
-			current = next;
+		while (currentNode != null) {
+			nextNode = currentNode.next;
+			currentNode.next = previousNode;
+			previousNode = currentNode;
+			currentNode = nextNode;
 		}
-
 		System.out.println("Linked List reversed successfully!");
 
-		return previous;
+		return previousNode;
 	}
 
-	// watch video for this
+	// Refer
+	// https://www.youtube.com/watch?v=DYpEpZzNmiA&list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&index=36
+
 	public ListNode getMiddleNode() {
 		if (head == null) {
 			return null;
@@ -225,7 +238,9 @@ public class SinglyLinkedList {
 		return slowPtr;
 	}
 
-	// watch video for this
+	// Refer
+	// https://www.youtube.com/watch?v=adS-s2Stg_A&list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&index=37
+
 	public ListNode getNthNodeFromEnd(int n) {
 		if (head == null) {
 			return null;
@@ -239,7 +254,6 @@ public class SinglyLinkedList {
 		ListNode refPtr = head;
 
 		int count = 0;
-
 		while (count < n) {
 			if (refPtr == null) {
 				throw new IllegalArgumentException(n + " is greater than the number of nodes in list");
@@ -255,7 +269,29 @@ public class SinglyLinkedList {
 		return mainPtr;
 	}
 
-	// watch video for this
+	// Refer
+	// https://www.youtube.com/watch?v=0Hn18rUkZKY&list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&index=38
+
+	public void removeDuplicates() {
+		if (head == null) {
+			return;
+		}
+
+		ListNode currentNode = head;
+		while (currentNode != null && currentNode.next != null) {
+			if (currentNode.data == currentNode.next.data) {
+				currentNode.next = currentNode.next.next;
+			} else {
+				currentNode = currentNode.next;
+			}
+		}
+
+		System.out.println("Duplicate nodes removed!");
+	}
+
+	// Refer
+	// https://www.youtube.com/watch?v=u9igWutVc4o&list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&index=39
+
 	public ListNode insertInSortedList(int value) {
 		ListNode newNode = new ListNode(value);
 
@@ -263,42 +299,49 @@ public class SinglyLinkedList {
 			return newNode;
 		}
 
-		ListNode current = head;
-		ListNode temp = null;
+		ListNode currentNode = head;
+		ListNode previousNode = null;
 
-		while (current != null && current.data < newNode.data) {
-			temp = current;
-			current = current.next;
+		while (currentNode != null && currentNode.data < newNode.data) {
+			previousNode = currentNode;
+			currentNode = currentNode.next;
 		}
 
-		newNode.next = current;
-		temp.next = newNode;
+		newNode.next = currentNode;
+		previousNode.next = newNode;
+
+		System.out.println("New Node insterted successfully into Sorted Linked List");
+
 		return head;
 	}
 
-	// watch video for this
+	// Refer
+	// https://www.youtube.com/watch?v=8SoncjEIW4c&list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&index=40
+
 	public void deleteNode(int key) {
-		ListNode current = head;
-		ListNode temp = null;
+		ListNode currentNode = head;
+		ListNode previousNode = null;
 
-		if (current != null && current.data == key) {
-			head = current.next;
+		if (currentNode != null && currentNode.data == key) {
+			head = currentNode.next;
 			return;
 		}
 
-		while (current != null && current.data != key) {
-			temp = current;
-			current = current.next;
+		while (currentNode != null && currentNode.data != key) {
+			previousNode = currentNode;
+			currentNode = currentNode.next;
 		}
 
-		if (current == null) {
+		if (currentNode == null) {
 			return;
 		}
 
-		temp.next = current.next;
+		previousNode.next = currentNode.next;
 	}
 
-	// watch video for this
+	// Refer
+	// https://www.youtube.com/watch?v=rOpnLs0lXy0&list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&index=41
+
 	public boolean containsLoop() {
 		ListNode fastPtr = head;
 		ListNode slowPtr = head;
@@ -314,7 +357,9 @@ public class SinglyLinkedList {
 		return false;
 	}
 
-	// watch video for this
+	// Refer
+	// https://www.youtube.com/watch?v=yKNOZ8bPdRQ&list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&index=43
+
 	public ListNode startNodeInALoop() {
 		ListNode fastPtr = head;
 		ListNode slowPtr = head;
@@ -330,17 +375,21 @@ public class SinglyLinkedList {
 		return null;
 	}
 
-	// watch video for this
+	// Refer
+	// https://www.youtube.com/watch?v=yKNOZ8bPdRQ&list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&index=43
+
 	private ListNode getStartingNode(ListNode slowPtr) {
 		ListNode temp = head;
 		while (temp != slowPtr) {
 			temp = temp.next;
 			slowPtr = slowPtr.next;
 		}
-		return temp; // starting node of the loop
+		return temp;
 	}
 
-	// watch video for this
+	// Refer
+	// https://www.youtube.com/watch?v=LWDVM_kgvdE&list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&index=46
+
 	public void removeLoop() {
 		ListNode fastPtr = head;
 		ListNode slowPtr = head;
@@ -351,12 +400,15 @@ public class SinglyLinkedList {
 
 			if (fastPtr == slowPtr) {
 				removeLoop(slowPtr);
+				System.out.println("Loop removed from Linked List");
 				return;
 			}
 		}
 	}
 
-	// watch video for this
+	// Refer
+	// https://www.youtube.com/watch?v=LWDVM_kgvdE&list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&index=46
+
 	private void removeLoop(ListNode slowPtr) {
 		ListNode temp = head;
 		while (temp.next != slowPtr.next) {
@@ -366,7 +418,9 @@ public class SinglyLinkedList {
 		slowPtr.next = null;
 	}
 
-	// watch video for this
+	// Refer
+	// https://www.youtube.com/watch?v=rOpnLs0lXy0&list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&index=41
+
 	public void createALoopInLinkedList() {
 		ListNode first = new ListNode(1);
 		ListNode second = new ListNode(2);
@@ -382,6 +436,39 @@ public class SinglyLinkedList {
 		fourth.next = fifth;
 		fifth.next = sixth;
 		sixth.next = third;
+
+		System.out.println("A loop Linked List created successfully!");
+	}
+
+	// Refer
+	// https://www.geeksforgeeks.org/how-to-sort-a-linkedlist-in-java/
+
+	public void sortLinkedList() {
+		// Node current will point to head
+		ListNode current = head, index = null;
+		int temp;
+		if (head == null) {
+			return;
+		} else {
+			while (current != null) {
+				// Node index will point to node next to current
+				index = current.next;
+
+				while (index != null) {
+					// If current node's data is greater than index's node data, swap the data
+					// between them
+					if (current.data > index.data) {
+						temp = current.data;
+						current.data = index.data;
+						index.data = temp;
+					}
+
+					index = index.next;
+				}
+				current = current.next;
+			}
+		}
+		System.out.println("Linked List is sorted now!");
 	}
 
 	public static void main(String[] args) {
@@ -398,7 +485,10 @@ public class SinglyLinkedList {
 		sll.insertLast(17);
 		sll.display();
 
-		sll.insert(3, 999);
+		sll.insert(3, 99);
+		sll.display();
+
+		sll.insert(3, 77);
 		sll.display();
 
 		sll.deleteFirst();
@@ -413,7 +503,54 @@ public class SinglyLinkedList {
 		sll.find(100);
 		sll.find(12);
 
-		sll.reverse();
+		sll.display();
+
+		ListNode middleNode = sll.getMiddleNode();
+		System.out.println("Middle Node : " + middleNode.data);
+
+		ListNode nodeFromEnd = sll.getNthNodeFromEnd(3);
+		System.out.println("3rd Node from end : " + nodeFromEnd.data);
+
+		sll.insertFirst(15);
+		sll.insertFirst(17);
+		sll.insertLast(17);
+		sll.insertLast(15);
+
+		sll.display();
+		sll.sortLinkedList();
+		sll.display();
+
+		sll.display();
+		sll.insertInSortedList(30);
+		sll.display();
+
+		sll.removeDuplicates();
+		sll.display();
+
+		// Code to reverse a Linked List
+		ListNode firstNode = new ListNode(1);
+		ListNode secondNode = new ListNode(3);
+		ListNode thirdNode = new ListNode(5);
+		ListNode fourthNode = new ListNode(7);
+		ListNode fifthNode = new ListNode(9);
+		ListNode sixthNode = new ListNode(12);
+
+		ListNode head1 = firstNode;
+		firstNode.next = secondNode;
+		secondNode.next = thirdNode;
+		thirdNode.next = fourthNode;
+		fourthNode.next = fifthNode;
+		fifthNode.next = sixthNode;
+
+		sll.display(head1);
+
+		ListNode reverseHead = sll.reverse(head1);
+		sll.display(reverseHead);
+
+		sll.createALoopInLinkedList();
+		System.out.println("Contains a loop ? :" + sll.containsLoop());
+
+		sll.removeLoop();
 		sll.display();
 
 	}
