13bb33a625d40459e823a2e0a979fb38a9b8b79a
unknown <biradar.abhijit@gmail.com>
2022-11-25 05:51:26 +0530

    25-nov-2022 commit 1

--- com.dsa/src/main/java/com/dsa/sorting/BucketSort.java
+++ com.dsa/src/main/java/com/dsa/sorting/BucketSort.java
@@ -6,47 +6,57 @@ import java.util.LinkedList;
 import java.util.List;
 
 // Refer
+// https://www.youtube.com/watch?v=E9OccfF9mpI
+// https://www.youtube.com/watch?v=rNdTWHQMvOk&t=10s
+// https://github.com/nikoo28/java-solutions/blob/master/src/main/java/util/BucketSort.java
 // https://www.javatpoint.com/bucket-sort-in-java
 
-public class BucketSort {	
+public class BucketSort {
 	public void binSort(int[] array, int bucketSize) {
-		// creating a list of buckets for storing lists
+
+		// Step 1 : Create a bucket list
 		List<Integer>[] buckets = new List[bucketSize];
-		// Linked list with each bucket array index
-		// as there may be hash collision
+
+		// Step 2 : Associate each index in bucket array with a linked list
 		for (int i = 0; i < bucketSize; i++) {
 			buckets[i] = new LinkedList<Integer>();
 		}
-		// calculate the hash and assigns elements to the proper bucket
+
+		// Step 3 : Assign number from array to the proper bucket by using hash function
 		for (int num : array) {
 			buckets[hash(num, bucketSize)].add(num);
 		}
-		// iterate over the buckets and sorts the elements
+
+		// Step 5 : Sort buckets
 		for (List<Integer> bucket : buckets) {
-			// sorts the bucket
 			Collections.sort(bucket);
 		}
+
+		// Step 6 : Merge buckets to get sorted array
 		int index = 0;
-		// gethered the buckets after sorting
 		for (List<Integer> bucket : buckets) {
 			for (int num : bucket) {
-				array[index++] = num;
+				array[index] = num;
+				index++;
 			}
 		}
 	}
 
-	// distributing elements
-	private static int hash(int num, int bucketSize) {
+	// Step 4 : This function will generate and return hash value
+	private int hash(int num, int bucketSize) {
 		return num / bucketSize;
 	}
 
 	public static void main(String args[]) {
-		// array to be sort
 		int[] array = { 22, 45, 12, 8, 10, 6, 72, 81, 33, 18, 50, 14, 55, 0, 12, 55 };
-		System.out.println("Unsorted Array: " + Arrays.toString(array));
-		// calling the user-defined method to sort the array
+
+		System.out.println("Array before sorting: ");
+		System.out.println(Arrays.toString(array));
+
 		BucketSort bs = new BucketSort();
 		bs.binSort(array, 10);
-		System.out.println("Sorted Array: " + Arrays.toString(array));
+
+		System.out.println("Array after sorting: ");
+		System.out.println(Arrays.toString(array));
 	}
 }
