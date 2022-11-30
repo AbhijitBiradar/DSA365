8c90879e0f130dc138f0f5bddd9618171f8ef6e6
unknown <biradar.abhijit@gmail.com>
2022-11-30 08:15:20 +0530

    20-Nov-2022 commit 1

--- com.dsa/src/main/java/com/dsa/sorting/ShellSort.java
+++ com.dsa/src/main/java/com/dsa/sorting/ShellSort.java
@@ -3,39 +3,39 @@ package com.dsa.sorting;
 import java.util.Arrays;
 
 // Refer
-// https://www.programiz.com/dsa/shell-sort
-// https://www.youtube.com/watch?v=9crZRd8GPWM
-// https://www.youtube.com/watch?v=xtydR3vKw3g
-// https://www.youtube.com/watch?v=4fO6PnW1WSs
-// https://www.youtube.com/watch?v=ZtjCHnHTK5Q
-
-// https://www.geeksforgeeks.org/shellsort/
-// https://www.programiz.com/dsa/shell-sort
-// https://www.educative.io/answers/what-is-a-shell-sort
+// https://www.youtube.com/watch?v=408TQi6MWmI&list=PLIY8eNdw5tW_zX3OCzX7NJ8bL1p6pWfgG&index=40
+// https://www.youtube.com/watch?v=zXyEZQY9nKY&list=PLIY8eNdw5tW_zX3OCzX7NJ8bL1p6pWfgG&index=41
+// https://simplesnippets.tech/shell-sort-algorithm-with-c-code-sorting-algorithms-data-structures-algorithms/
+// https://www.youtube.com/watch?v=WEZVY6_cZvE
 
 class ShellSort {
 
-	// Rearrange elements at each n/2, n/4, n/8, ... intervals
-	void shellSort(int array[], int n) {
-		for (int interval = n / 2; interval > 0; interval /= 2) {
-			for (int i = interval; i < n; i += 1) {
-				int temp = array[i];
-				int j;
-				for (j = i; j >= interval && array[j - interval] > temp; j -= interval) {
-					array[j] = array[j - interval];
+	void shellSort(int arr[]) {
+		int size = arr.length;
+		for (int gap = size / 2; gap > 0; gap = gap / 2) {
+
+			for (int j = gap; j < size; j = j + 1) {
+				int temp = arr[j];
+				int i = 0;
+
+				for (i = j; (i >= gap) && (arr[i - gap] > temp); i = i - gap) {
+					arr[i] = arr[i - gap];
 				}
-				array[j] = temp;
+				arr[i] = temp;
 			}
 		}
 	}
 
-	// Driver code
 	public static void main(String args[]) {
-		int[] data = { 9, 8, 3, 7, 5, 6, 4, 1 };
-		int size = data.length;
+		int[] arr = { 9, 8, 3, 7, 5, 6, 4, 1 };
+
+		System.out.println("Array before sorting: ");
+		System.out.println(Arrays.toString(arr));
+
 		ShellSort ss = new ShellSort();
-		ss.shellSort(data, size);
-		System.out.println("Sorted Array in Ascending Order: ");
-		System.out.println(Arrays.toString(data));
+		ss.shellSort(arr);
+
+		System.out.println("Array after sorting: ");
+		System.out.println(Arrays.toString(arr));
 	}
 }
